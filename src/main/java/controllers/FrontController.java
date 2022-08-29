c50475f21721ea61cab4d35da33337a12dc3a51b
yuusuke.kumano <kuma704kuma@gmail.com>
2022-08-29 15:33:29 +0900

    日報indexアクションの作成

--- src/main/java/controllers/FrontController.java
+++ src/main/java/controllers/FrontController.java
@@ -18,7 +18,7 @@ import constants.ForwardConst;
  *
  */
 @WebServlet("/")
-public class FrontController extends HttpServlet{
+public class FrontController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     public FrontController() {
@@ -28,56 +28,60 @@ public class FrontController extends HttpServlet{
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request,HttpServletResponse response)
-        throws ServletException, IOException{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
         //パラメータに該当するActionクラスのインスタンス
-        ActionBase action = getAction(request,response);
+        ActionBase action = getAction(request, response);
 
         //サーブレットコンテキスト、リクエスト、レスポンスをActionインスタンスのフィールドに設定
-        action.init(getServletContext(),request,response);
+        action.init(getServletContext(), request, response);
 
         //Actionクラスの処理を呼び出し
         action.process();
     }
 
+
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException{
+            throws ServletException, IOException {
         doGet(request, response);
     }
 
     /**
      * リクエストパラメータの値から該当するActionクラスのインスタンスを作成し、返却する
-     * (例：パラメータがaction=Employeeの場合、actions.EmployeeActionオブジェクト)
+     * (例:パラメータが action=Employee の場合、actions.EmployeeActionオブジェクト)
      * @param request リクエスト
      * @param response レスポンス
      * @return
      */
-    @SuppressWarnings({"rawtypes", "unchecked"}) // コンパイラ警告を抑制
+    @SuppressWarnings({ "rawtypes", "unchecked" }) //コンパイラ警告を抑制
     private ActionBase getAction(HttpServletRequest request, HttpServletResponse response) {
         Class type = null;
         ActionBase action = null;
         try {
 
-            //リクエストからパラメータ"action"の値を取得（例："Employee"、"Report"）
+            //リクエストからパラメータ"action"の値を取得 (例:"Employee"、"Report")
             String actionString = request.getParameter(ForwardConst.ACT.getValue());
 
-            //該当するActionオブジェクトを作成（例：リクエストからパラメータ action=Employeeの場合、actions.EmployeeActionオブジェクト）
+            //該当するActionオブジェクトを作成 (例:リクエストからパラメータ action=Employee の場合、actions.EmployeeActionオブジェクト)
             type = Class.forName(String.format("actions.%sAction", actionString));
 
-            //ActionBaseのオブジェクトにキャスト（例：actions.EmployeeActionオブジェクト→actions.ActionBaseオブジェクト）
-            action = (ActionBase)(type.asSubclass(ActionBase.class)
+            //ActionBaseのオブジェクトにキャスト(例:actions.EmployeeActionオブジェクト→actions.ActionBaseオブジェクト)
+            action = (ActionBase) (type.asSubclass(ActionBase.class)
                     .getDeclaredConstructor()
                     .newInstance());
-        }catch(ClassNotFoundException | InstantiationException | IllegalAccessException | SecurityException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException e) {
 
-            //リクエストパラメータに設定されている"action"の値が不正の場合（例：action = xxxxx等、該当するActionクラスがない場合）
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SecurityException
+                | IllegalArgumentException | InvocationTargetException| NoSuchMethodException e) {
+
+            //リクエストパラメータに設定されている"action"の値が不正の場合(例:action=xxxxx 等、該当するActionクラスがない場合)
             //エラー処理を行うActionオブジェクトを作成
             action = new UnknownAction();
         }
         return action;
     }
+
 }
